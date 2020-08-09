86bb3a1eafcb1067ff8551510fe581a7c035dc71
Guilherme Silva <44985027+mrguibs@users.noreply.github.com>
2020-08-09 19:00:27 -0300

    Create new file

---
+++ banking_ruby.rb
@@ -0,0 +1,62 @@
+class Account
+  attr_reader :name
+  attr_reader :balance
+  def initialize(name, balance=100)
+    @name = name
+    @balance = balance
+  end
+  
+  public
+  def display_balance(pin_number)
+    if pin_number == pin
+      puts "Balance: $#{@balance}."
+    else
+      puts pin_error
+    end
+  end
+
+  def withdraw(pin_number, amount)
+
+    if pin_number == pin
+      if amount > @balance
+        puts error_checking
+      else
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance $#{@balance}."
+      end
+    else
+    puts pin_error
+    end
+  end
+
+def deposit(pin_number, amount)
+  if pin_number == pin
+  @balance += amount
+  puts "Deposited #{amount}. New balance $#{@balance}."
+  else
+  puts pin_error
+  end
+end
+
+  private
+  def pin
+    @pin = 1234
+  end
+  def pin_error
+    return "Access denied: incorrect PIN."
+  end 
+
+  def error_checking
+  return "It was not possible to withdraw. Your balance is #{@balance}."
+  end
+end
+
+
+checking_account = Account.new("Guilherme", 1_000_000)
+
+checking_account.display_balance(1234)
+
+checking_account.withdraw(1234,1_100_000)
+checking_account.withdraw(1234,500_000)
+checking_account.deposit(1234, 100)collaborate,
+without friction
